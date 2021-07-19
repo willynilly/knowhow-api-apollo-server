21fa7d8c81763b55e76ca29a8a5cb7e8911e797e
Will Riley <will@willriley.net>
2021-07-19 00:27:01 -0400

    Removed printing to console.

--- graphql/resolvers/reviewResolvers.js
+++ graphql/resolvers/reviewResolvers.js
@@ -1,22 +1,16 @@
 const reviewResolvers = {
   badge: (parent, args, context, info) => {
     let badgeService = context.dataSources.knowHowAPI.badgeService;
-    console.log("review badge");
-    console.log(args);
     return badgeService.findById(parent.badge_id);
   },
 
   requester: (parent, args, context, info) => {
     let userService = context.dataSources.knowHowAPI.userService;
-    console.log("review requester user");
-    console.log(args);
     return userService.findById(parent.requester_user_id);
   },
 
   reviewer: (parent, args, context, info) => {
     let userService = context.dataSources.knowHowAPI.userService;
-    console.log("review reviewer user");
-    console.log(args);
     return userService.findById(parent.reviewer_user_id);
   },
 };
