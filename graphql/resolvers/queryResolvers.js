6760bb37d17d3b953fa26d199427a59b2423dc99
Will Riley <will@willriley.net>
2021-07-19 00:28:27 -0400

    Deleted logging controller methods to console.

--- graphql/resolvers/queryResolvers.js
+++ graphql/resolvers/queryResolvers.js
@@ -1,56 +1,38 @@
 const queryResolvers = {
   users: async (parent, args, context, info) => {
     let userService = context.dataSources.knowHowAPI.userService;
-    console.log("users");
-    console.log(args);
     return userService.findAll();
   },
   user: async (parent, args, context, info) => {
     let userService = context.dataSources.knowHowAPI.userService;
-    console.log("user");
-    console.log(args);
     return userService.findById(args.id);
   },
   badges: async (parent, args, context, info) => {
     let badgeService = context.dataSources.knowHowAPI.badgeService;
-    console.log("badges");
-    console.log(args);
     return badgeService.findAll();
   },
   badge: async (parent, args, context, info) => {
     let badgeService = context.dataSources.knowHowAPI.badgeService;
-    console.log("badge");
-    console.log(args);
     return badgeService.findById(args.id);
   },
   badgesByAuthor: async (parent, args, context, info) => {
     let badgeService = context.dataSources.knowHowAPI.badgeService;
-    console.log("badgesBuAuthor");
-    console.log(args);
     return badgeService.findByAuthorUserId(args.author_user_id);
   },
   reviews: async (parent, args, context, info) => {
     let reviewService = context.dataSources.knowHowAPI.reviewService;
-    console.log("reviews");
-    console.log(args);
     return reviewService.findAll();
   },
   review: async (parent, args, context, info) => {
     let reviewService = context.dataSources.knowHowAPI.reviewService;
-    console.log("review");
-    console.log(args);
     return reviewService.findById(args.id);
   },
   reviewsByRequester: async (parent, args, context, info) => {
     let reviewService = context.dataSources.knowHowAPI.reviewService;
-    console.log("reviewsByRequester");
-    console.log(args);
     return reviewService.findByRequesterUserId(args.requester_user_id);
   },
   reviewsByReviewer: async (parent, args, context, info) => {
     let reviewService = context.dataSources.knowHowAPI.reviewService;
-    console.log("reviewsByRequester");
-    console.log(args);
     return reviewService.findByReviewerUserId(args.reviewer_user_id);
   },
 };
